6b1ae2717ac660a0ec59c1e75f9b390615df111a
toosyou <toosyou.tw@gmail.com>
2018-03-29 17:31:59 +0800

    add sgd optimizer with momentum and nesterov support

--- mlp.py
+++ mlp.py
@@ -5,8 +5,6 @@ import sys
 import sklearn
 from sklearn import datasets
 from sklearn.preprocessing import OneHotEncoder
-import autograd.numpy as grad_np
-from autograd import elementwise_grad as egrad
 
 def normalized(a, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
@@ -93,14 +91,7 @@ class Loss:
         t = np.array(t)
         return np.vectorize(lambda xi, ti: -( ti * np.log(xi+epslion) + (1.-ti) * np.log(1.-xi-epslion) ))(x, t).sum()
 
-    @staticmethod
-    def cross_entropy_dx(x, t, epslion=1e-10):
-        ''' return the differential of cross_entropy by autograd since it's not differentialable when x=0 '''
-        x = grad_np.array(x)
-        t = grad_np.array(t)
-        def ce(x):
-            return -(t*grad_np.log(x) + (1.-t)*grad_np.log(1.-x))
-        return egrad(ce)(x)
+    # cross_entropy_dx simply doesn't work alone
 
     @staticmethod
     def softmax_cross_entropy(x, t):
@@ -121,6 +112,34 @@ class Loss:
         x = np.array(x)
         t = np.array(t)
         return x-t
+
+class Optimizer:
+    def __init__(self): pass
+
+    class sgd:
+        def __init__(self, momentum=0., nesterov=False):
+            self.last_updates_w = 0
+            self.last_updates_b = 0
+            self.last_grad_w = 0
+            self.last_grad_b = 0
+            self.momentum = momentum
+            self.nesterov = nesterov
+
+        def cal_updates(self, grad_w, grad_b, lr):
+            if self.nesterov:
+                updates_w = ( grad_w + self.momentum*(self.last_updates_w + grad_w - self.last_grad_w) ) * lr
+                updates_b = ( grad_b + self.momentum*(self.last_updates_b + grad_b - self.last_grad_b) ) * lr
+            else:
+                updates_w = (grad_w + self.momentum * self.last_updates_w) * lr
+                updates_b = (grad_b + self.momentum * self.last_updates_b) * lr
+
+            # update last weight and bias
+            self.last_grad_w = grad_w
+            self.last_grad_b = grad_b
+            self.last_updates_w = updates_w
+            self.last_updates_b = updates_b
+
+            return updates_w, updates_b
 
 class MLPClassifier:
     class __fc_layer:
@@ -196,26 +215,36 @@ class MLPClassifier:
             self,
             hidden_layer_sizes=128,
             n_hidden_layers=10,
-            activation='relu', solver='sgd',
-            loss='softmax_cross_entropy',
+            activation='relu',
+            solver='sgd',
+            momentum=0.,
+            nesterov=False,
+            loss='cross_entropy',
             weight_initializer='orthogonal',
             bias_initializer='zeros',
             batch_size=16,
             learning_rate=0.01,
-            learning_rate_decay=0.8
+            learning_rate_decay=0.95
         ):
+        loss_need_convert = ['cross_entropy']
+
         self.hidden_layer_sizes = hidden_layer_sizes
         self.batch_size = batch_size
         self.hidden_layers = None
         self.output_layer = None
         self.activation = activation
-        self.loss = getattr(Loss, loss.lower())
-        self.loss_dx = getattr(Loss, loss.lower()+'_dx')
         self.weight_initializer = weight_initializer
         self.bias_initializer = bias_initializer
         self.learning_rate = learning_rate
         self.n_hidden_layers = n_hidden_layers
         self.learning_rate_decay = learning_rate_decay
+        self.optimizer = getattr(Optimizer, solver.lower())(momentum=momentum, nesterov=nesterov)
+
+        # softmax + cross_entropy -> softmax_cross_entropy
+        self.output_layer_activation = 'linear' if loss.lower() in loss_need_convert else 'softmax'
+        loss = 'softmax_' + loss.lower() if loss.lower() in loss_need_convert else loss
+        self.loss = getattr(Loss, loss.lower())
+        self.loss_dx = getattr(Loss, loss.lower()+'_dx')
 
         self.input_dim = -1
         self.output_dim = -1
@@ -226,12 +255,8 @@ class MLPClassifier:
             rtn = h(rtn)
         return self.output_layer(rtn)
 
-    def __loss(self, predict_y, true_y):
-        return self.loss(predict_y, true_y)
-
     def __backpropagation(self, predict_y, true_y):
         loss_dx = self.loss_dx(predict_y, true_y)
-        # print('bp', predict_y, true_y, loss_dx)
         return self.output_layer.backpropagation(loss_dx) # all gradient
 
     def __update(self, updates_w, updates_b):
@@ -255,7 +280,18 @@ class MLPClassifier:
                                     bias_initializer=self.bias_initializer
                                 ))
 
-    def fit(self, X, y, epochs=100, shuffle=True):
+    def __accuracy_loss(self, X, y):
+        ''' return total accuracy and average loss '''
+        loss = 0.
+        n_currect = 0
+        X = np.array(X)
+        predict_y = self.predict(X)
+        for py, yi in zip(predict_y, y):
+            if np.argmax(py) == np.argmax(yi): n_currect += 1
+            loss += self.loss(py, yi)
+        return n_currect / X.shape[0], loss / X.shape[0]
+
+    def fit(self, X, y, valid_set=None, epochs=100, shuffle=True):
         X = np.array(X) # (?, input_dim)
         y = np.array(y) # (?, output_dim)
 
@@ -270,38 +306,50 @@ class MLPClassifier:
         self.output_layer = self.__fc_layer(
                                     input_layer=self.hidden_layers[-1] if len(self.hidden_layers) else self.input_dim,
                                     n_unit=self.output_dim,
-                                    activation='linear',
+                                    activation=self.output_layer_activation,
                                     weight_initializer=self.weight_initializer,
                                     bias_initializer=self.bias_initializer
                                 )
 
-        for e in range(epochs):
+        for index_epoch in range(epochs):
             if shuffle: X, y = sklearn.utils.shuffle(X, y)
             for batch_x, batch_y in self.__batchlize(X, y):
                 # SGD
                 grad_w_sum = None
                 grad_b_sum = None
-                loss_sum = 0.
                 for xi, yi in zip(batch_x, batch_y):
                     predict_y = self.__forward_pass(xi)
                     grads_w, grads_b = self.__backpropagation(predict_y, yi)
                     grad_w_sum = grads_w if grad_w_sum is None else grad_w_sum + grads_w
                     grad_b_sum = grads_b if grad_b_sum is None else grad_b_sum + grads_b
-                    loss_sum += self.__loss(predict_y, yi)
                 # calculate updates
-                grad_w_avg = grad_w_sum / self.batch_size * self.learning_rate
-                grad_b_avg = grad_b_sum / self.batch_size * self.learning_rate
-                loss_avg = loss_sum / self.batch_size
-                self.__update(grad_w_avg, grad_b_avg)
+                grad_w_avg = grad_w_sum / self.batch_size
+                grad_b_avg = grad_b_sum / self.batch_size
 
-            # self.learning_rate *= self.learning_rate_decay
+                updates_w, updates_b = self.optimizer.cal_updates(grad_w_avg, grad_b_avg, self.learning_rate)
+                self.__update(updates_w, updates_b)
+
+            # update learning rate
+            self.learning_rate *= self.learning_rate_decay
+
             # result
-            print(e, self.__forward_pass(X[0]), y[0], loss_avg)
+            train_acc, train_loss = self.__accuracy_loss(X[:self.batch_size], y[:self.batch_size])
+            if valid_set is not None:
+                valid_acc, valid_loss = self.__accuracy_loss(valid_set[0], valid_set[1])
+
+            print('epochs: %d/%d lr: %.6f' % (index_epoch, epochs, self.learning_rate), end=' ')
+            print('loss: %.4f acc: %.4f ' % (train_loss, train_acc), end='\n' if valid_set is None else '')
+            if valid_set is not None:
+                print('valid_loss: %.4f valid_acc: %.4f' % (valid_loss, valid_acc))
 
         return None
 
-    def predict(X):
-        pass
+    def predict(self, X):
+        rtn = list()
+        for x in X:
+            if self.output_layer_activation == 'linear':
+                rtn.append(Activation.softmax(self.__forward_pass(x)))
+        return np.array(rtn)
 
 if __name__ == '__main__':
     iris = datasets.load_iris()
@@ -310,8 +358,10 @@ if __name__ == '__main__':
 
     X, y = sklearn.utils.shuffle(X, y)
 
-    # X = X[0:1]
-    # y = y[0:1]
+    # split to training and validation set
+    train_ratio = 0.7
+    train_x, train_y = X[:int(X.shape[0]*train_ratio)], y[:int(y.shape[0]*train_ratio)]
+    valid_x, valid_y = X[int(X.shape[0]*train_ratio):], y[int(y.shape[0]*train_ratio):]
 
-    mlp_clf = MLPClassifier(batch_size=min(16, y.shape[0]))
-    mlp_clf.fit(X, y, epochs=10000)
+    mlp_clf = MLPClassifier(batch_size=min(16, y.shape[0]), momentum=0.4, nesterov=True)
+    mlp_clf.fit(train_x, train_y, valid_set=(valid_x, valid_y), epochs=10000)
